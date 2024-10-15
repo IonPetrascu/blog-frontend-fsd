e26f8815da4c23161ef490977a2391c513642eb3
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-10-15 10:52:27 +0300

    feat: add video component to display video

---
+++ src/shared/ui/TheVideo.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+defineProps<{
+  src: string,
+
+}>()
+
+</script>
+<template>
+  <video class="video" :src="src" controls>
+    Video not found
+  </video>
+</template>
+<style scoped>
+.video {
+  width: 50%;
+  border-radius: 10px;
+  margin-top: 50px;
+}
+</style>
