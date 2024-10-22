7b00172306337a6b961d8706f21a87b2f3b048db
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-10-22 16:38:37 +0300

    feat: delete chat

---
+++ src/pages/chats/ui/ChatSettings.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const emit = defineEmits<{
+  (e: 'deleteChat'): void
+}>()
+
+</script>
+<template>
+  <div class="wrapper">
+    <div>
+      <button @click="emit('deleteChat')">Delete chat </button>
+    </div>
+  </div>
+</template>
+<style scoped></style>
